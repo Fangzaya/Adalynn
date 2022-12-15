97196153b773eae5ac8c41c3701de7fbd03a2a6a
atittaya <tunaatittaya@gmail.com>
2022-12-14 22:52:24 -0500

    let's go!!

--- src/Hey.java
+++ src/Hey.java
@@ -1,5 +1,7 @@
 public class Hey {
     public static void main(String[] args) {
         System.out.println("Hello beautiful");
+        System.out.println("---");
+        System.out.println("it is time to sleep");
     }
 }
